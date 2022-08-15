dce55b5d50b56ba3bb6ebdf405f04d624c7df036
David Aghassi <3680126+Aghassi@users.noreply.github.com>
2022-08-15 10:22:44 -0400

    chore: dump brew installs and add xcode check
    Adds brew install line to setup
    Dumps existing brew installs from current machine
    Adds xcode check for install

--- setup.sh
+++ setup.sh
@@ -5,5 +5,18 @@ BREW_INSTALLED=$(which brew)
 
 # If brew is not installed, install it
 if [ -z "$BREW_INSTALLED" ]; then
+    # https://gist.github.com/ChristopherA/a579274536aab36ea9966f301ff14f3f
+    # Installs xcode commandline tools but not xcode itself
+    # It is assumed that if brew is not installed then Xcode most likely
+    # is not either and that we must install it too for brew to work
+    if [$(uname -s) == "Darwin"]; then
+        touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+        softwareupdate -i -a
+        rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    fi
+
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+HOMEBREW_BREWFILE=~/.Brewfile
+brew bundle install --global --no-lock --cleanup
